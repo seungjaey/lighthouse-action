9a1e7042eb5f7f51fe7d1c63469f56f981ac53c4
seungjae.yuk <seungjae.yuk@kurlycorp.com>
2022-07-15 11:42:06 +0900

    [fix] import github

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import github, { getOctokit } from '@actions/github'
+import * as github from '@actions/github'
 import log from './logger'
 import parseInput from './utils/parseInput'
 
@@ -7,8 +7,8 @@ async function run(): Promise<void> {
   try {
     log('StartUp')
     const input = parseInput()
-    const { ghToken } = input
-    const { context } = github
+    const {ghToken} = input
+    const {context,getOctokit} = github
     const pull_number = context.payload.pull_request?.number || 0
     log('--------')
     log(github.context)
